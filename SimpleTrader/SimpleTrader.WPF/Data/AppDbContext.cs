dbc53c1ba2c1a23026bb49aa2524763240dc860f
Gamil Yassin <gamil.yassin@gmail.com>
2025-06-19 14:59:51 +0300

    Update AppDbContext.cs

--- SimpleTrader/SimpleTrader.WPF/Data/AppDbContext.cs
+++ SimpleTrader/SimpleTrader.WPF/Data/AppDbContext.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using FieldOps.Kernel.AppService;
+using Microsoft.EntityFrameworkCore;
 using SimpleTrader.WPF.Domain.Models;
+using Microsoft.Extensions.Logging;
+using SimpleTrader.WPF.Domain.ModelBuilder;
 
 namespace SimpleTrader.WPF.Data;
 
@@ -8,12 +12,21 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Account> Accounts { get; set; }
     public DbSet<AssetTransaction> AssetTransactions { get; set; }
-    public AppDbContext(DbContextOptions options) : base(options) { }
+    // public AppDbContext(DbContextOptions options) : base(options) { }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(new ApplicationService().GetDefaultConnectString())
+            .LogTo(Console.WriteLine, LogLevel.Information);
+        base.OnConfiguring(options);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<AssetTransaction>()
-            .OwnsOne(a => a.Asset);
+        modelBuilder.ConfigureUsers()
+            .ConfigureAccounts()
+            .ConfigureAssetTransactions()
+            .ConfigureMajorIndecies();
 
         base.OnModelCreating(modelBuilder);
     }
