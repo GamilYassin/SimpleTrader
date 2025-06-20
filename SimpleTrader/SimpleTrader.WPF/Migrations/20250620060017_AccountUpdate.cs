e63a4b86383a133ef0e30acaeb51eb3788751fc7
Gamil Yassin <gamil.yassin@gmail.com>
2025-06-20 10:02:44 +0300

    Migration and db update

---
+++ SimpleTrader/SimpleTrader.WPF/Migrations/20250620060017_AccountUpdate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleTrader.WPF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Accounts");
+        }
+    }
+}
